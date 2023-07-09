dd6991fa5e430e071ea2ecb14805e2f5ae22ea85
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-07-09 09:11:53 +0300

    initialzie

---
+++ app/src/main/java/com/example/session_1speedtraining/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.session_1speedtraining
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
