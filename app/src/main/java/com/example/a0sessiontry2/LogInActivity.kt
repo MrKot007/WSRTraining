17eb34698dffb054c944b9eca432afe8c4f8d75f
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-07-09 19:32:21 +0300

    bugFixNowWorking

--- app/src/main/java/com/example/a0sessiontry2/LogInActivity.kt
+++ app/src/main/java/com/example/a0sessiontry2/LogInActivity.kt
@@ -2,10 +2,13 @@ package com.example.a0sessiontry2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a0sessiontry2.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
