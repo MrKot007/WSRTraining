4daf3e9e7dd06ed488d63cce95d1af6cad1c3a94
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-07-10 09:46:14 +0300

    boardingViewDone

--- app/src/main/java/com/example/a0sessiontry3/MainActivity.kt
+++ app/src/main/java/com/example/a0sessiontry3/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.a0sessiontry3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a0sessiontry3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        controller.fillQueue()
+        val element = controller.getElement()
+        binding.image.setImageResource(element.third)
+        binding.heading.text = element.first
+        binding.paragraph.text = element.second
+
+        binding.next.setOnClickListener {
+            controller.removeElement()
+            if (controller.getSize() == 1) {
+                
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/a0sessiontry3/QueueController.kt
@@ -0,0 +1,32 @@
+package com.example.a0sessiontry3
+
+import java.util.LinkedList
+import java.util.Queue
+
+class QueueController {
+    val queue: Queue<Triple<String, String, Int>> = LinkedList()
+
+    fun getSize() : Int{
+        return 0
+    }
+
+    fun getElement() : Triple<String, String, Int> {
+        return Triple("", "", 0)
+    }
+
+    fun addElement(element: Triple<String, String, Int>) {
+
+    }
+
+    fun removeElement() {
+
+    }
+
+    fun fillQueue() {
+
+    }
+
+    fun clearQueue() {
+
+    }
+}
